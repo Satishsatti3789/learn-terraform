fd769885101f1b68db6f9b98d29f40d5fed40c58
Satishsatti3789 <satishdevops3789@gmail.com>
2024-09-13 14:19:19 +0530

    Update main.tf

--- roboshop-v1/ec2/main.tf
+++ roboshop-v1/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.centos.id
+  ami                    = data.aws_ami.example.id
   instance_type          = "t3.small"
   vpc_security_group_ids = [aws_security_group.sg.id]
 
@@ -10,37 +10,7 @@ resource "aws_instance" "web" {
 }
 
 resource "null_resource" "ansible" {
-  depends_on = [aws_instance.web, aws_route53_record.roboshop]
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.web.public_ip
-    }
-
-    inline = [
-      "sudo labauto ansible",
-      "ansible-pull -i localhost, -U https://github.com/satishsatti3789/ansible-v1 main.yml -e env=dev -e role_name=${var.name}"
-    ]
-  }
-}
-
-resource "aws_route53_record" "roboshop" {
-  zone_id = "resource "aws_instance" "web" {
-  ami                    = data.aws_ami.centos.id
-  instance_type          = "t3.small"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-
-  tags = {
-    Name = var.name
-  }
-
-}
-
-resource "null_resource" "ansible" {
-  depends_on = [aws_instance.web, aws_route53_record.roboshop]
+  depends_on = [aws_instance.web, aws_route53_record.www]
   provisioner "remote-exec" {
 
     connection {
@@ -57,75 +27,8 @@ resource "null_resource" "ansible" {
   }
 }
 
-resource "aws_route53_record" "roboshop" {
-  zone_id = "resource "aws_instance" "web" {
-  ami                    = data.aws_ami.centos.id
-  instance_type          = "t3.small"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-
-  tags = {
-    Name = var.name
-  }
-
-}
-
-resource "null_resource" "ansible" {
-  depends_on = [aws_instance.web, aws_route53_record.roboshop]
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.web.public_ip
-    }
-
-    inline = [
-      "sudo labauto ansible",
-      "ansible-pull -i localhost, -U https://github.com/raghudevopsb73/roboshop-ansible main.yml -e env=dev -e role_name=${var.name}"
-    ]
-  }
-}
-
-resource "aws_route53_record" "roboshop" {
-  zone_id = "Z014790038ULCSA62ANIV"
-  name    = "${var.name}-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.web.private_ip]
-}
-
-data "aws_ami" "centos" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
-
-
-resource "aws_security_group" "sg" {
-  name        = var.name
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = var.name
-  }
-}
-
-variable "name" {}"
+resource "aws_route53_record" "www" {
+  zone_id = "Z055331734ICV430E01P7"
   name    = "${var.name}-dev"
   type    = "A"
   ttl     = 30
@@ -133,43 +36,6 @@ variable "name" {}"
 }
 
 data "aws_ami" "example" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
-
-
-resource "aws_security_group" "sg" {
-  name        = var.name
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = var.name
-  }
-}
-
-variable "name" {}"
-  name    = "${var.name}-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.web.private_ip]
-}
-
-data "aws_ami" "centos" {
   owners      = ["973714476881"]
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
