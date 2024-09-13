bf603ac35078c97110f09ea261d93dec4a348958
Satishsatti3789 <satishdevops3789@gmail.com>
2024-09-13 16:15:30 +0530

    Update main.tf

--- roboshop-v1/ec2/main.tf
+++ roboshop-v1/ec2/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.example.id
   instance_type          = "t3.small"
   vpc_security_group_ids = [aws_security_group.sg.id]
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
 
   tags = {
